5246c14e082c9307d29eec28f64033ccd50d8973
Sanakhatun <professional.developer.tutorials@gmail.com>
2020-02-09 01:37:57 +0530

    Adding BaseActivity

---
+++ app/src/main/java/com/sana/kotlinwithretrofit/common/BaseActivity.kt
@@ -0,0 +1,31 @@
+package com.sana.kotlinwithretrofit.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import com.sana.kotlinwithretrofit.R
+
+open class BaseActivity : AppCompatActivity() {
+
+    private var toolbar: Toolbar? = null
+    private var tv_title: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+        init()
+    }
+
+    open fun init(){
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+}
